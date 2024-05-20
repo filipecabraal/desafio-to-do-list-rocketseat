2220fc900b871557b6bf4e698cde4e2e7ab09c02
Filipe Cabral <filipecabraal@gmail.com>
2024-05-20 11:35:00 -0300

    :sparkles: feat(app): Iniciando estrutura do layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { Header } from './components/Header';
+import styles from './App.module.scss';
 import './styles/global.scss';
+
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
       <Header />
 
-      <h2>ToDo List</h2>
+      <main>
+        <article className={styles.wrap}>
+          <form action="">
+            <input type="text" placeholder='Adicione uma nova tarefa' />
+            <button type='submit'>Criar</button>
+          </form>
+
+          <div>
+            <header>
+              <p>Tarefas criadas<span></span></p>
+              <p>Concluídas<span></span></p>
+            </header>
+
+            <div>
+              <img src="" alt="" />
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+
+          </div>
+        </article>
+      </main>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-import './Header.module.scss';
+import styles from './Header.module.scss';
 
 import logo from '../assets/logo.svg'
 
 export function Header() {
     return(
         <>
-            <header>
+            <header className={styles.header}>
                 <img src={logo} alt="Logotipo rocket to-do list desafio Rocketseat" />
             </header>
         </>
